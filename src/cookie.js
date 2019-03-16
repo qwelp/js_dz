ecfa43aac8cf9baf95be957c966fcefdb75c979e
qwelp <qwelp@mail.ru>
2019-03-16 20:35:20 +0300

    Задание 7 end
    Signed-off-by: Vladimir Bun <qwelp@mail.ru>

--- src/cookie.js
+++ src/cookie.js
@@ -36,17 +36,121 @@ const homeworkContainer = document.querySelector('#homework-container');
 const filterNameInput = homeworkContainer.querySelector('#filter-name-input');
 // текстовое поле с именем cookie
 const addNameInput = homeworkContainer.querySelector('#add-name-input');
-// текстовое поле со значением cookie
+// текстовое поле со значением cookie 
 const addValueInput = homeworkContainer.querySelector('#add-value-input');
 // кнопка "добавить cookie"
 const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
 
-filterNameInput.addEventListener('keyup', function() {
-    // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
+filterNameInput.addEventListener('keyup', function(e) {
+    const inputValue = e.target.value;
+
+    let
+        items = itemsCookie(),
+        htmlTr = '',
+        obj = {};
+
+    for (let item in items) {
+        if (isMatching(item, inputValue) || isMatching(items[item], inputValue)) {
+            obj[item] = items[item];
+        }
+    }
+
+    listTable.innerHTML = '';
+
+    for (let key in obj) {
+        htmlTr += `<tr><td>${key}</td><td>${items[key]}</td><td><a class="delete-cookie" href="#">Удалить</a></td><tr>`;
+    }
+
+    listTable.innerHTML = htmlTr;
+});
+
+const itemsCookie = () => {
+
+    const arCookie = document.cookie.split('; ');
+
+    const objCookie = arCookie.reduce((obj, v) => {
+
+        const [name, value] = v.split('=');
+
+        obj[name] = value;
+
+        return obj;
+    }, {});
+
+    return objCookie;
+};
+
+const itemsCreateTr = () => {
+
+    let
+        items = itemsCookie(),
+        htmlTr = '';
+
+    listTable.innerHTML = '';
+
+    for (let key in items) {
+        htmlTr += `<tr><td>${key}</td><td>${items[key]}</td><td><a class="delete-cookie" href="#">Удалить</a></td></tr>`;
+    }
+
+    listTable.innerHTML = htmlTr;
+};
+
+const addCookie = () => {
+
+    let items = listTable.children,
+        flag = true;
+
+    for (i = 0; i < items.length; i++) {
+        if (items[i].childNodes.length > 0) {
+            if (items[i].children[0].textContent === addNameInput.value
+                && itemsCookie()[items[i].children[0].textContent] !== addValueInput.value)
+            {
+                items[i].remove();
+                flag = false;
+            }
+        }
+    }
+
+    document.cookie = `${addNameInput.value}=${addValueInput.value}`;
+
+    addNameInput.value = '';
+    addValueInput.value = '';
+
+    if (flag) {
+        itemsCreateTr();
+    }
+}
+
+const deleteCookie = cookie_name => {
+    const date = new Date ( );
+    date.setTime (date.getTime() - 1);
+    document.cookie = cookie_name += "=; expires=" + date.toGMTString();
+};
+
+const isMatching = (full, chunk) => {
+    if (full.toLowerCase().indexOf(chunk.toLowerCase()) >= 0) {
+        return true;
+    }
+    return false;
+};
+
+document.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target.getAttribute('class') === 'delete-cookie') {
+
+        const
+            tr = target.closest('tr'),
+            name = tr.children[0].textContent;
+
+        deleteCookie(name);
+        itemsCreateTr();
+    }
 });
 
 addButton.addEventListener('click', () => {
-    // здесь можно обработать нажатие на кнопку "добавить cookie"
+    addCookie();
 });
+
+itemsCreateTr();
